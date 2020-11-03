97224ce2e720f292b236933a2714939fb39af5e1
Charley Ferguson <charley@pcc.com>
2020-11-03 16:25:17 -0500

    Clicking navbar title brings you to home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { useHistory } from "react-router-dom";
 
 function Navbar({ title }) {
+  const history = useHistory();
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <Typography variant="h4">{title}</Typography>
+        <Typography variant="h4" onClick={() => history.push("/")}>
+          {title}
+        </Typography>
       </Toolbar>
     </AppBar>
   );
