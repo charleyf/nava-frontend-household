db8b03c0a08e6a4bd4e3cb29e6c30382497e2856
Charley Ferguson <charley@pcc.com>
2020-11-03 14:17:15 -0500

    Add the add button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Pageheader from "./components/Pageheader";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         title={"Your household"}
         description={"Welcome to The Marketplace! Review your household below:"}
       />
+      <Button variant="contained" color="primary" onClick={() => {}}>
+        Add new member
+      </Button>
     </>
   );
 }
